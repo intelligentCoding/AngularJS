0d34e32fe6a9dfe198ed8c2883efb73e7480200b
Kashif Mahmood <31515792+intelligentCoding@users.noreply.github.com>
2021-10-31 15:11:04 -0600

    Using services instead of factories

---
+++ services/app/controllers/customersController.js
@@ -0,0 +1,23 @@
+(function() {
+    
+    var CustomersController = function ($scope, customersService) {
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
+        
+        function init() {
+            $scope.customers = customersService.getCustomers();
+        }
+        init();
+        $scope.doSort = function(propName) {
+           $scope.sortBy = propName;
+           $scope.reverse = !$scope.reverse;
+        };
+        
+    };
+    
+    CustomersController.$inject = ['$scope', 'customersService'];
+
+    angular.module('customersApp')
+      .controller('CustomersController', CustomersController);
+    
+}());
